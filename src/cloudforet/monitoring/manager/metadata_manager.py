d6233c2aaf8d471a58255c5fe8f4f68bc4002cad
LimChaeyeon <cylim@mz.co.kr>
2025-10-26 15:13:52 +0900

    add search filter

--- src/cloudforet/monitoring/manager/metadata_manager.py
+++ src/cloudforet/monitoring/manager/metadata_manager.py
@@ -7,6 +7,7 @@ from cloudforet.monitoring.model.metadata.metadata_dynamic_field import (
     DateTimeDyField,
     EnumDyField,
     MoreField,
+    SearchField,
     TextDyField,
 )
 
@@ -19,7 +20,7 @@ class MetadataManager(BaseManager):
 
     @staticmethod
     def get_data_source_metadata():
-        metadata = LogMetadata.set_fields(
+        metadata = LogMetadata.set_meta(
             name="oci-logging-table",
             fields=[
                 MoreField.data_source(
@@ -73,6 +74,55 @@ class MetadataManager(BaseManager):
                     },
                 ),
                 TextDyField.data_source("Message", "log_content.data.message"),
+            ],
+            search=[
+                SearchField.set(name="Event Name", key="log_content.data.eventName"),
+                SearchField.set(
+                    name="Status",
+                    key="log_content.data.response.status",
+                    enums={
+                        "200": {"label": "OK"},
+                        "201": {"label": "Created"},
+                        "202": {"label": "Accepted"},
+                        "204": {"label": "No Content"},
+                        "400": {"label": "Bad Request"},
+                        "401": {"label": "Unauthorized"},
+                        "403": {"label": "Forbidden"},
+                        "404": {"label": "Not Found"},
+                        "409": {"label": "Conflict"},
+                        "412": {"label": "Precondition Failed"},
+                        "422": {"label": "Unprocessable Entity"},
+                        "429": {"label": "Too Many Requests"},
+                        "500": {"label": "Internal Server Error"},
+                        "502": {"label": "Bad Gateway"},
+                        "503": {"label": "Service Unavailable"},
+                        "504": {"label": "Gateway Timeout"},
+                        "507": {"label": "Insufficient Storage"},
+                    },
+                ),
+                SearchField.set(
+                    name="User Name", key="log_content.data.identity.principalName"
+                ),
+                SearchField.set(
+                    name="Event Time",
+                    key="log_content.time",
+                    data_type="datetime",
+                ),
+                SearchField.set(
+                    name="Action",
+                    key="log_content.data.request.action",
+                    enums={
+                        "GET": {"label": "GET"},
+                        "POST": {"label": "POST"},
+                        "PUT": {"label": "PUT"},
+                        "PATCH": {"label": "PATCH"},
+                        "DELETE": {"label": "DELETE"},
+                    },
+                ),
+                SearchField.set(
+                    name="Message",
+                    key="log_content.data.message",
+                ),
             ],
         )
         return metadata
